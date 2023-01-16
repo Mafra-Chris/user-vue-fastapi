95ab2d2f27b33466c889edea1d64ae4733acd515
Christiano <mafradesouza.cs@gmail.com>
2023-01-16 14:05:58 -0300

    feat: 404 page

--- services/frontend/src/pages/[...all].vue
+++ services/frontend/src/pages/[...all].vue
@@ -1,1 +1,8 @@
-<template>404</template>
+<template>
+  <div class="h-screen flex justify-center">
+    <img
+      class="w-2/3"
+      src="../assets/404-error-with-a-cute-animal-animate.svg"
+    />
+  </div>
+</template>
